6ab3e87ba4552cc28b52d31e6dbc5aba0d780c1f
SbShinoda <tuantt.stb@gmail.com>
2026-05-27 15:44:20 +0700

    Add sanitize and debounce utility functions

--- assets/js/helpers.js
+++ assets/js/helpers.js
@@ -1,1 +1,13 @@
+function sanitize(str) {
+  const div = document.createElement('div');
+  div.textContent = str;
+  return div.innerHTML;
+}
 
+function debounce(fn, delay) {
+  let timer;
+  return function(...args) {
+    clearTimeout(timer);
+    timer = setTimeout(() => fn.apply(this, args), delay);
+  };
+}

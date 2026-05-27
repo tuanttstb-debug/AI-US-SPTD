9d34d270b69e687b7fc211c2507433b14b2d1882
SbShinoda <tuantt.stb@gmail.com>
2026-05-27 15:34:50 +0700

    Update env.js

--- assets/config/env.js
+++ assets/config/env.js
@@ -1,1 +1,6 @@
-
+const APP_CONFIG = {
+  API_BASE_URL: 'https://script.google.com/macros/s/AKfycby...your-deployment-id.../exec', // Replace with real URL
+  VERSION: '1.0.0',
+  AUTO_SAVE_KEY: 'ai_usecase_draft',
+  DUPLICATE_THRESHOLD: 0.8
+};

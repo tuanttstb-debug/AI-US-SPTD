f8a3415946d9b5a14a81b012e2bfe403c1b09540
SbShinoda <tuantt.stb@gmail.com>
2026-05-27 15:45:58 +0700

    Add Toast component for displaying messages

--- assets/js/toast.js
+++ assets/js/toast.js
@@ -1,1 +1,10 @@
-
+const Toast = {
+  show(message, type = 'info') {
+    const container = document.getElementById('toastContainer');
+    const toast = document.createElement('div');
+    toast.className = `toast toast-${type}`;
+    toast.textContent = message;
+    container.appendChild(toast);
+    setTimeout(() => toast.remove(), 4000);
+  }
+};

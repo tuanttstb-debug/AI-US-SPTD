112114aac3a8e9d0648a7eb9d46b58e142c9bfab
SbShinoda <tuantt.stb@gmail.com>
2026-05-28 10:29:14 +0700

    Update env.js

--- config/env.js
+++ config/env.js
@@ -1,5 +1,5 @@
 var APP_CONFIG = {
-  API_BASE_URL: 'https://script.google.com/macros/s/AKfycbyaM1dQcCZYHNam3zb6UrwP5Qf8BnsJr1XzjPUuGqla-k2WCAI5llLllIhadU7mfBfP/exec/', // Replace with real URL
+  API_BASE_URL: 'https://script.google.com/macros/s/AKfycbwVGwFLjqGetD4WTkQ2OiMUG4TOUjEfr9zQ5I2zhjNWZ2_aJHlqGmytsjSy6je9zniU/exec/', // Replace with real URL
   VERSION: '1.0.0',
   AUTO_SAVE_KEY: 'ai_usecase_draft',
   DUPLICATE_THRESHOLD: 0.8

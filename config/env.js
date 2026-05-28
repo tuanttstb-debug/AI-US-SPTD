915ef97986f27afc430d2fd7a476c271b4a8ed52
SbShinoda <tuantt.stb@gmail.com>
2026-05-28 15:27:56 +0700

    Update env.js

--- config/env.js
+++ config/env.js
@@ -1,7 +1,7 @@
 var APP_CONFIG = {
   // URL GAS Web App — KHÔNG có trailing slash
   // Cập nhật mỗi khi deploy lại GAS (mỗi lần "New Deployment" sẽ ra URL mới)
-  API_BASE_URL: 'https://script.google.com/macros/s/AKfycbycXcBAVEzDvpiKN7cQWnNLxWQC6QLm9rx1zUo9xC5jbWRrtwePYlQ9dKWzh12ca0zdcQ/exec',
+  API_BASE_URL: 'https://script.google.com/macros/s/AKfycbzndk-pef0V627bVBhJjBzJUs-Jtgkll8-u5cldTsElB2hmx8gd-uwGDmZBpbmYh4MimQ/exec',
   VERSION: '1.0.0',
   AUTO_SAVE_KEY: 'ai_usecase_draft',
   DUPLICATE_THRESHOLD: 0.8

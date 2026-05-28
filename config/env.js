453d8cb123190a2e059aa74882ef3a54a5feb071
SbShinoda <tuantt.stb@gmail.com>
2026-05-28 10:27:16 +0700

    Update env.js

--- config/env.js
+++ config/env.js
@@ -1,4 +1,4 @@
-const APP_CONFIG = {
+var APP_CONFIG = {
   API_BASE_URL: 'https://script.google.com/macros/s/AKfycbyaM1dQcCZYHNam3zb6UrwP5Qf8BnsJr1XzjPUuGqla-k2WCAI5llLllIhadU7mfBfP/exec/', // Replace with real URL
   VERSION: '1.0.0',
   AUTO_SAVE_KEY: 'ai_usecase_draft',
